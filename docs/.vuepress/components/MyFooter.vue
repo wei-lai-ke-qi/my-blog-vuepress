340993f58fbf6bff6dfa970454ec441e7f3a2a85
wei-lai-ke-qi <wei-lai-ke-qi@users.noreply.github.com>
2026-05-07 19:25:21 +0800

    修改页脚

--- docs/.vuepress/components/MyFooter.vue
+++ docs/.vuepress/components/MyFooter.vue
@@ -11,7 +11,7 @@ import Resume from "../../More/Resume.vue";
       <div class="links">
 <!--        没有贡献者-->
 <!--        <a href="" target="_blank" rel="noopener noreferrer">贡献者</a>-->
-            <resume />
+<!--            <resume />-->
 
 <!--        <a href="/More/About.md" class="about-link">关于本站</a>-->
         <RouterLink to="/More/About.html" class="about-link">关于本站</RouterLink>
